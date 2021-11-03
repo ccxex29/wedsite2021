07e6600a1b1a192961049bf6b15821a86ab32cef
ccxex29 <blackccxex@gmail.com>
2021-11-04 02:03:41 +0700

    Fix apostrophe errors

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,8 @@ import NProgress from 'nprogress';
 // @ts-ignore
 import Plx from 'react-plx';
 
+const someStoryText =
+"Grace and Fecund first met in university through a campus event in Binus University. Grace was a freshman, and Fecund was a sophomore at that time.  After Grace's first year, she accidentally met Fecund again, and after seven months of going out together, they finally in a relationship. They used to wait for each other to meet after class to have lunch together, try new cafes or eat their favourite foods. Also, sometimes they did school's projects together. They had different hobbies like Grace adores classical music, so she takes Fecund to watch classical concerts and introduces many songs. She also loves travelling, she asked him to go with her, tried going to new destinations and now, he can't wait to go to more places with her. Fecund likes films, so he often takes Grace to the cinema. He is more capable of design; it makes him help and teaches her a lot of it. Fun fact, he is neater, cleaner and calmer than Grace. But, Grace is well-managed and able to think faster. So, they ideally fit and help each other. After almost ten years and cherishing every moment together, they decided to take a new step in their life to start a new family."
 
 const Home = (): JSX.Element => {
     NProgress.done();
@@ -110,7 +112,7 @@ const Home = (): JSX.Element => {
                     paddingInline: '6rem',
                     paddingBottom: '4rem',
                 }}>
-                    Grace and Fecund first met in university through a campus event in Binus University. Grace was a freshman, and Fecund was a sophomore at that time.  After Grace's first year, she accidentally met Fecund again, and after seven months of going out together, they finally in a relationship. They used to wait for each other to meet after class to have lunch together, try new cafes or eat their favourite foods. Also, sometimes they did school's projects together. They had different hobbies like Grace adores classical music, so she takes Fecund to watch classical concerts and introduces many songs. She also loves travelling, she asked him to go with her, tried going to new destinations and now, he can't wait to go to more places with her. Fecund likes films, so he often takes Grace to the cinema. He is more capable of design; it makes him help and teaches her a lot of it. Fun fact, he is neater, cleaner and calmer than Grace. But, Grace is well-managed and able to think faster. So, they ideally fit and help each other. After almost ten years and cherishing every moment together, they decided to take a new step in their life to start a new family.
+                    {someStoryText}
                 </p>
                 <TimelineView dimensions={dimensions} />
             </section>
