b2cb21bd6ae5f797d9b898be610541e526848b4b
ccxex29 <blackccxex@gmail.com>
2021-11-04 01:50:25 +0700

    Fix dev mode checking on webpack

--- next.config.js
+++ next.config.js
@@ -33,8 +33,8 @@ module.exports = {
 			},
 		],
 	],
-	webpack: (config, {isDev, isServer}) => {
-		if (!isDev && !isServer) {
+	webpack: (config, {dev, isServer}) => {
+		if (!dev && !isServer) {
 			Object.assign(config.resolve.alias, {
 				'react': 'preact/compat',
 				'react-dom/test-utils': 'preact/test-utils',
