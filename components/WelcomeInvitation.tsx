ee462cd2ecc10538f660d7cb2e5428702a8bd321
ccxex29 <blackccxex@gmail.com>
2021-11-10 22:16:38 +0700

    Add break on welcome invitation text

--- components/WelcomeInvitation.tsx
+++ components/WelcomeInvitation.tsx
@@ -1,7 +1,7 @@
 const WelcomeInvitation = () => {
     return (
         <h2 aria-label={'Welcome Invitation'} style={{fontSize: '1.4em'}}>
-            Wherever you are, <b>we delightfully invite you to witness our vows</b> and share the joy with us virtually
+            Wherever you are, <b>we delightfully invite you<br/>to witness our vows</b> and share the joy with us virtually
         </h2>
     );
 }
